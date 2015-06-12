6ee6009a95ce083c66c59886f89bb9b1e41d454c
Essensea24 <sophie.chor24@gmail.com>
2015-06-12 15:54:08 -0700

    added searches controller with index new create and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'searches/index'
+
+  get 'searches/new'
+
+  get 'searches/create'
+
+  get 'searches/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

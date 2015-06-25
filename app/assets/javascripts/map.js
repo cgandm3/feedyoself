8a1b6eb9b6edc8c17787b98e79cc37abdae674bf
Chris Garvin <chris.garvin@me.com>
2015-06-24 18:49:10 -0700

    changed marker images on map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,13 +2,10 @@ var prices = []
 var cuisines = []
 
 $(document).ready(function(){
-  var menuItem = document.getElementById("menuItem").innerHTML;
-  console.log(menuItem);
     $("#submit").click(function(){
       prices.push(document.getElementById("price").value);
       cuisines.push(document.getElementById("exampleInputName2").value);
       getLocation();
-      console.log(prices);
     });
     prices.push(document.getElementById("price").value);
     cuisines.push(document.getElementById("exampleInputName2").value);
@@ -35,45 +32,35 @@ function getLocation() {
 function showPosition(position) {
 
 // grabbing input values and location values to be used in ajax post for new search item
-    console.log(menuItem);
     var y = prices[prices.length - 1];
     var z = cuisines[cuisines.length -1];
     var userLongitude = position.coords.longitude;
     var userLatitude = position.coords.latitude;
-    var placeLongitude = document.getElementById("longitude").innerText;
-    var placeLatitude = document.getElementById("latitude").innerText;
-    var restaurantName = document.getElementById("restaurantName").innerText;
-    var placeZip = document.getElementById("postalCode").innerText;
-
-    console.log(y);
-    console.log(userLongitude);
-    console.log(placeLatitude);
-    console.log(placeZip);
 
 // finally calling ajax function to post data to new search object
 
-        ajax(y,z,userLatitude,userLongitude,placeZip);
-          location.reload(true);
+        ajax(y,z,userLatitude,userLongitude);
+        window.location.assign("/searches/show")
+          // location.reload(true);
 
 
 }
 
 // ajax function to post input data to new search object to be accessible by rails
 
-function ajax(price,cuisine,lat,long,zip){
+function ajax(price,cuisine,lat,long){
 
   $.ajax({
     type: "POST",
     url: "/searches",
     dataType: 'json',
-    data: { search: {cuisine: cuisine, price: price, latitude: lat, longitude: long, location: zip}},
+    data: { search: {cuisine: cuisine, price: price, latitude: lat, longitude: long}},
   });
 
 
 }
 
 function reloadLocation() {
-  console.log("hi")
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPositionAgain);
     } else {
@@ -84,27 +71,19 @@ function reloadLocation() {
 // if geolocation is available, runs showPosition function
 
 function showPositionAgain(position) {
-    console.log("hello")
 // grabbing input values and location values to be used in ajax post for new search item
-    console.log(menuItem.innerText);
     var y = prices[prices.length - 1];
     var z = cuisines[cuisines.length -1];
     var userLongitude = position.coords.longitude;
     var userLatitude = position.coords.latitude;
-    var placeLongitude = document.getElementById("longitude").innerText;
-    var placeLatitude = document.getElementById("latitude").innerText;
-    var restaurantName = document.getElementById("restaurantName").innerText;
-    var placeZip = document.getElementById("postalCode").innerText;
 
     console.log(y);
     console.log(userLongitude);
     console.log(placeLatitude);
-    console.log(placeZip);
 
     var bounds = new google.maps.LatLngBounds();
 
         var myCenter = new google.maps.LatLng(userLatitude,userLongitude);
-        var myCenter2 = new google.maps.LatLng(placeLatitude,placeLongitude);
 
         var marker;
 
@@ -122,21 +101,9 @@ function showPositionAgain(position) {
           position:myCenter,
         });
 
+        marker2.setIcon('http://www.thatscoop.com/app/webroot/img/emoticons/happy.png')
+
         marker2.setMap(map);
-
-        var marker = new google.maps.Marker({
-          position:myCenter2
-
-        });
-
-        marker.setMap(map);
-
-// extending the bounds of the map to show both current location and restaraunt
-
-        bounds.extend(marker.position);
-        bounds.extend(marker2.position);
-
-        map.fitBounds(bounds);
 
         var infowindow = new google.maps.InfoWindow({
               content: "ITS YOU"
@@ -144,13 +111,37 @@ function showPositionAgain(position) {
 
             infowindow.open(map,marker2);
 
-var contentString = '<h3>' + restaurantName + '</h3>' + '<h4>' + menuItem.innerText + '</h4>'
+    for(var i = 0; i < 3; i++){
+        var menuItem = document.getElementsByClassName("menuItem")[i].innerText;
+        var placeLongitude = document.getElementsByClassName("longitude")[i].innerText;
+        var placeLatitude = document.getElementsByClassName("latitude")[i].innerText;
+        var restaurantName = document.getElementsByClassName("restaurantName")[i].innerText;
+        var myCenter2 = new google.maps.LatLng(placeLatitude,placeLongitude);
+
+        var marker = new google.maps.Marker({
+          position:myCenter2
+
+    });
+        marker.setIcon('http://www.flawlessmilano.com/private/wp-content/themes/flawless/images/food-icon.png')
+
+        marker.setMap(map);
+        bounds.extend(marker.position);
+
+        var contentString = '<h5>' + restaurantName + '</h5>' + '<p>' + menuItem + '</p>';
 
         var infowindow = new google.maps.InfoWindow({
               content: contentString
             });
 
             infowindow.open(map,marker);
+      }
+
+// extending the bounds of the map to show both current location and restaraunt
+        bounds.extend(marker2.position);
+
+        map.fitBounds(bounds);
+
+
 
 // finally calling ajax function to post data to new search object
 

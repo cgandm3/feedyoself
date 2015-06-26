e4cb9286e9d3a80bb1375da2e58d619f1ef9a23e
Chris Garvin <chris.garvin@me.com>
2015-06-25 21:15:11 -0700

    took some ruby out of show view and put inside search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -24,11 +24,24 @@ class SearchesController < ApplicationController
   def create
     @search = Search.new(params.require(:search).permit(:location, :cuisine, :price, :latitude, :longitude))
     @search.save
-    raise @search.inspect
+    # raise @search.inspect
 
     if @search.save
       render searches_path
     end
+  end
+
+  def show
+
+    latitude = Search.last.latitude
+    longitude = Search.last.longitude
+    @response = HTTParty.get"https://api.locu.com/v1_0/venue/search/?location=#{latitude}%2C#{longitude}&has_menu=TRUE&radius=2000&category=restaurant&api_key=7a3583e12a33f7edc1b626c5fdbba42a266d11c4"
+    @placeLat = []
+    @placeLong = []
+    @placeZip = []
+    @restaurantName = []
+    @randomItem = []
+
   end
 
   def about
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,5 @@
 class Search < ActiveRecord::Base
-	validates :location, :price, :latitude, :longitude, presence: true
-	validates :price, numericality: true
-	validates :cuisine, format: { with: /\A^(?![1-9]\d*)$\z/i }, presence: true 
-	
+	# validates :location, :price, :latitude, :longitude, presence: true
+	# validates :price, numericality: true
+	# validates :cuisine, format: { with: /\A^(?![1-9]\d*)$\z/i }, presence: true
 end

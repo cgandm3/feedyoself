e1d3250800287acf9ba076bf3ee59ccae2802be0
Chris Garvin <chris.garvin@me.com>
2015-06-28 17:42:42 -0700

    added bounce animation to users location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,37 +1,37 @@
 var prices = [];
 
-$(document).ready(function(){
-    $("#submit").click(function(){
-      if(isNaN(document.getElementById("price").value)){
+$(document).ready(function() {
+  $("#submit").click(function() {
+    if (isNaN(document.getElementById("price").value)) {
+      $(".error").text("Please enter a valid dollar amount.");
+    } else if (document.getElementById("price").value.trim().length < 1) {
+      $(".error").text("Please enter a valid dollar amount.");
+    } else {
+      $(".error").text("");
+      $('#rotating-image').hide();
+      $(".loadingPizza").show();
+      prices.push(document.getElementById("price").value);
+      getLocation();
+    }
+  });
+  $('.form-control').keypress(function(e) {
+    if (e.which == 13) {
+      if (isNaN(document.getElementById("price").value)) {
         $(".error").text("Please enter a valid dollar amount.");
-      } else if(document.getElementById("price").value.trim().length < 1){
+      } else if (document.getElementById("price").value.trim().length < 1) {
         $(".error").text("Please enter a valid dollar amount.");
       } else {
         $(".error").text("");
         $('#rotating-image').hide();
         $(".loadingPizza").show();
-      prices.push(document.getElementById("price").value);
-      getLocation();
-    }
-    });
-    $('.form-control').keypress(function (e) {
-      if (e.which == 13) {
-        if(isNaN(document.getElementById("price").value)){
-          $(".error").text("Please enter a valid dollar amount.");
-        } else if(document.getElementById("price").value.trim().length < 1){
-          $(".error").text("Please enter a valid dollar amount.");
-        } else {
-          $(".error").text("");
-          $('#rotating-image').hide();
-          $(".loadingPizza").show();
         prices.push(document.getElementById("price").value);
         getLocation();
       }
-        return false;    //<---- Add this line
-      }
-    });
-    prices.push(document.getElementById("price").value);
-    showPositionAgain();
+      return false; //<---- Add this line
+    }
+  });
+  prices.push(document.getElementById("price").value);
+  showPositionAgain();
 });
 
 
@@ -42,11 +42,11 @@ var x = document.getElementById("demo");
 // is available
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 
 // if geolocation is available, runs showPosition function which grabs data to
@@ -54,34 +54,40 @@ function getLocation() {
 
 function showPosition(position) {
 
-// grabbing input values and location values to be used in ajax post for new search item
-    var y = prices[prices.length - 1];
-    var userLongitude = position.coords.longitude;
-    var userLatitude = position.coords.latitude;
+  // grabbing input values and location values to be used in ajax post for new search item
+  var y = prices[prices.length - 1];
+  var userLongitude = position.coords.longitude;
+  var userLatitude = position.coords.latitude;
 
-// calling ajax function to post data to new search object
+  // calling ajax function to post data to new search object
 
-        ajax(y,userLatitude,userLongitude);
+  ajax(y, userLatitude, userLongitude);
 
-// getting the last id in order to redirect to the proper search show page
+  // getting the last id in order to redirect to the proper search show page
 
-        $.get("/searches.json")
-        .done(function(data){
-          var page = data[data.length-1].id;
-          window.location.assign("/searches/" + page);
-        })
+  $.get("/searches.json")
+    .done(function(data) {
+      var page = data[data.length - 1].id;
+      window.location.assign("/searches/" + page);
+    })
 
 }
 
 // ajax function to post input data to new search object to be accessible by rails
 
-function ajax(price,lat,long){
+function ajax(price, lat, long) {
 
   $.ajax({
     type: "POST",
     url: "/searches",
     dataType: 'json',
-    data: { search: {price: price, latitude: lat, longitude: long}},
+    data: {
+      search: {
+        price: price,
+        latitude: lat,
+        longitude: long
+      }
+    },
   });
 
 }
@@ -99,103 +105,165 @@ function ajax(price,lat,long){
 function showPositionAgain() {
 
   $.get("/searches.json")
-  .done(function(data){
+    .done(function(data) {
 
-    var y = prices[prices.length - 1];
-    var userLongitude = data[data.length-1].longitude;
-    var userLatitude = data[data.length-1].latitude;
+      var y = prices[prices.length - 1];
+      var userLongitude = data[data.length - 1].longitude;
+      var userLatitude = data[data.length - 1].latitude;
 
-    console.log(y);
-    console.log(userLongitude);
-    console.log(placeLatitude);
+      console.log(y);
+      console.log(userLongitude);
+      console.log(placeLatitude);
 
-    var bounds = new google.maps.LatLngBounds();
+      var bounds = new google.maps.LatLngBounds();
 
-        var myCenter = new google.maps.LatLng(userLatitude,userLongitude);
+      var myCenter = new google.maps.LatLng(userLatitude, userLongitude);
 
-        var marker;
+      var marker;
 
-        var mapProp = {
-          center: myCenter,
-          zoom:10,
-          zoomControl:false,
-          panControl:false,
-          mapTypeId:google.maps.MapTypeId.ROAD,
-          styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke",
-          "stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text",
-          "stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon",
-          "stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text",
-          "stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon",
-          "stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry",
-          "stylers":[{"visibility":"simplified"}]},{"featureType":"water",
-          "stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill",
-          "stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text",
-          "stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon",
-          "stylers":[{"gamma":1},{"saturation":50}]}],
-        };
+      var mapProp = {
+        center: myCenter,
+        zoom: 10,
+        zoomControl: false,
+        panControl: false,
+        mapTypeId: google.maps.MapTypeId.ROAD,
+        styles: [{
+          "stylers": [{
+            "saturation": -100
+          }, {
+            "gamma": 1
+          }]
+        }, {
+          "elementType": "labels.text.stroke",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.business",
+          "elementType": "labels.text",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.business",
+          "elementType": "labels.icon",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.place_of_worship",
+          "elementType": "labels.text",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.place_of_worship",
+          "elementType": "labels.icon",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [{
+            "visibility": "simplified"
+          }]
+        }, {
+          "featureType": "water",
+          "stylers": [{
+            "visibility": "on"
+          }, {
+            "saturation": 50
+          }, {
+            "gamma": 0
+          }, {
+            "hue": "#50a5d1"
+          }]
+        }, {
+          "featureType": "administrative.neighborhood",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#333333"
+          }]
+        }, {
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [{
+            "weight": 0.5
+          }, {
+            "color": "#333333"
+          }]
+        }, {
+          "featureType": "transit.station",
+          "elementType": "labels.icon",
+          "stylers": [{
+            "gamma": 1
+          }, {
+            "saturation": 50
+          }]
+        }],
+      };
 
 
-        var map = new google.maps.Map(document.getElementById("map"),mapProp);
+      var map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-        var marker2=new google.maps.Marker({
-          position:myCenter,
-        });
+      var marker2 = new google.maps.Marker({
+        position: myCenter,
+      });
 
-        marker2.setIcon('http://www.thatscoop.com/app/webroot/img/emoticons/happy.png');
+      marker2.setIcon('http://www.thatscoop.com/app/webroot/img/emoticons/happy.png');
+      marker2.setAnimation(google.maps.Animation.BOUNCE);
 
-        marker2.setMap(map);
+      marker2.setMap(map);
 
-    for(var i = 0; i < 3; i++){
+      for (var i = 0; i < 3; i++) {
 
         var menuItem = document.getElementsByClassName("menuItem")[i].innerText;
         var placeLongitude = document.getElementsByClassName("longitude")[i].innerText;
         var placeLatitude = document.getElementsByClassName("latitude")[i].innerText;
         var restaurantName = document.getElementsByClassName("restaurantName")[i].innerText;
-        var myCenter2 = new google.maps.LatLng(placeLatitude,placeLongitude);
+        var myCenter2 = new google.maps.LatLng(placeLatitude, placeLongitude);
         var address = document.getElementsByClassName("address")[i].innerText;
         var phoneNumber = document.getElementsByClassName("phoneNumber")[i].innerText;
 
         var marker = new google.maps.Marker({
-          position:myCenter2
+          position: myCenter2
 
-    });
-        marker.setIcon('http://www.flawlessmilano.com/private/wp-content/themes/flawless/images/food-icon.png')
-
+        });
+        marker.setIcon('http://www.flawlessmilano.com/private/wp-content/themes/flawless/images/food-icon.png');
         marker.setMap(map);
         bounds.extend(marker.position);
 
         // google.maps.event.addListener(marker, 'click', function(){});
 
-        var contentString = '<div id="mapWindow"><h5>' + restaurantName + '</h5>' + '<p>' + menuItem + '</p>' +
-        '<a class="infoLink">more info</a>'+'<div class="additionalInfo" style="display:none;">'+ address + '<br>' + phoneNumber + '</div></div>';
+        var contentString = '<div id="mapWindow"><h5>' + restaurantName + '</h5>' + '<p>' + menuItem + '</p>'
+          + '<div class="additionalInfo">' + address + '<br>' + phoneNumber + '</div></div>';
 
         var infowindow = new google.maps.InfoWindow({
-              content: contentString
-            });
+          content: contentString,
+          hideCloseButton: true
+        });
 
-    //         google.maps.event.addListener(marker, 'mouseover', function() {
-    //     infowindow.open(map, this);
-    // });
-    //
-    // // assuming you also want to hide the infowindow when user mouses-out
-    // google.maps.event.addListener(marker, 'mouseout', function() {
-    //     infowindow.close();
-    // });
+        infowindow.open(map, marker);
 
-            infowindow.open(map,marker);
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
 
       }
 
 
-// extending the bounds of the map to show both current location and restaraunt
-        bounds.extend(marker2.position);
+      // extending the bounds of the map to show both current location and restaraunt
+      bounds.extend(marker2.position);
 
-        map.fitBounds(bounds);
+      map.fitBounds(bounds);
 
-        $("#map").css({'background':'white'});
+      $("#map").css({
+        'background': 'white'
+      });
 
 
-  })
+    })
 
 
 
